827a7cb9077ddbc68bd7ffb972af489f58f0e9d5
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-12-14 17:32:52 -0500

    feat: update routing to include dashboard and home components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import {ProductListComponent} from './features/inventory/components/product-list/product-list.component';
+import { HomeComponent } from './shared/components/home/home.component';
 
 const productRoutes: Routes = [
   {path: 'list', component: ProductListComponent}
 ];
 
 const allRoutes: Routes = [
-  {path: 'products', children: productRoutes}
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard',component: HomeComponent},
+  {path: 'products', children: productRoutes},
 ]
 
 export const routes: Routes = [
