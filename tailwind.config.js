573471dbf076bf9ee5340970f64aff97e0c55865
Miguel Angel Ybañez Esquerre <109547479+Miguel080902@users.noreply.github.com>
2024-12-14 15:48:39 -0500

    feat: sidebar and toolbar scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
       fontFamily: {
         sans: ['"Nunito Sans"', '"Roboto"', 'sans-serif'],
       },
+      colors: {
+        // Colores personalizados
+        lightBg: '#f3f4f6', // Fondo en modo claro
+        darkBg: '#1f2937',  // Fondo en modo oscuro
+        lightText: '#374151',
+        darkText: '#f9fafb',
+      },
     },
   },
   plugins: [],
