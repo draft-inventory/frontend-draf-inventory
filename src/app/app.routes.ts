1eaa93c8fc815fb5a98ef2ef4a4641fdd9d3ad2b
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-12-14 17:22:28 -0500

    chore: add product routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {ProductListComponent} from './features/inventory/components/product-list/product-list.component';
 
-export const routes: Routes = [];
+const productRoutes: Routes = [
+  {path: 'list', component: ProductListComponent}
+];
+
+const allRoutes: Routes = [
+  {path: 'products', children: productRoutes}
+]
+
+export const routes: Routes = [
+  ...allRoutes,
+];
+
+
