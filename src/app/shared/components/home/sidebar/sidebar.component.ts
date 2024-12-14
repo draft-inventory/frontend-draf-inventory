45cc291e831be924015666b25e7c5fa86eed85a0
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-12-14 18:48:54 -0500

    feat: update sidebar component to display dynamic navigation options

--- src/app/shared/components/home/sidebar/sidebar.component.ts
+++ src/app/shared/components/home/sidebar/sidebar.component.ts
@@ -1,13 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListOptionsComponent } from '../list-options/list-options.component';
+import { RouterModule } from '@angular/router';
+
 @Component({
-    selector: 'app-sidebar',
-    imports: [CommonModule, ListOptionsComponent],
-    templateUrl: './sidebar.component.html',
-    standalone: true,
-    styleUrl: './sidebar.component.css'
+  selector: 'app-sidebar',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+  standalone: true,
 })
 export class SidebarComponent {
   @Input() isOpen: boolean = true;
+
+  options = [
+    { name: 'Dashboard', icon: '🏠', route: '/dashboard' },
+    { name: 'Products', icon: '📦', route: '/products/list' },
+    { name: 'Favorites', icon: '❤️', route: '#' },
+    { name: 'Inbox', icon: '📨', route: '#' },
+    { name: 'Order Lists', icon: '📝', route: '#' },
+    { name: 'Product Stock', icon: '📊', route: '#' },
+  ];
 }
