9bdffd419e54904d059f249ca9ee905c1bc882ad
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-12-14 18:26:55 -0500

    feat: enhance routing structure to include dashboard component and update home template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,21 @@
 import { Routes } from '@angular/router';
-import {ProductListComponent} from './features/inventory/components/product-list/product-list.component';
+import { ProductListComponent } from './features/inventory/components/product-list/product-list.component';
 import { HomeComponent } from './shared/components/home/home.component';
+import { DashboardComponent } from './shared/components/dashboard/dashboard.component';
 
 const productRoutes: Routes = [
-  {path: 'list', component: ProductListComponent}
+  { path: 'list', component: ProductListComponent },
 ];
-
-const allRoutes: Routes = [
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard',component: HomeComponent},
-  {path: 'products', children: productRoutes},
-]
 
 export const routes: Routes = [
-  ...allRoutes,
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'products', children: productRoutes },
+    ],
+  },
+  { path: '**', redirectTo: 'dashboard' },
 ];
-
-
--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [
     NavbarComponent,
     SidebarComponent,
-    CommonModule
+    CommonModule,
+    RouterOutlet,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
