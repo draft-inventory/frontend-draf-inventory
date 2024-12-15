4e5452e4d41ca62ef4df4e87d921ef239a79b4c4
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-12-14 19:25:13 -0500

    feat: implement dark mode support and update navbar and sidebar styles

--- src/app/shared/components/home/navbar/navbar.component.ts
+++ src/app/shared/components/home/navbar/navbar.component.ts
@@ -1,23 +1,42 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [
-    CommonModule
-  ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
   standalone: true
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   @Output() sidebarToggle = new EventEmitter<void>();
+  currentTheme: string = 'light';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      //  Definimos el tema actual en función de la configuración del sistema
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      this.currentTheme = prefersDark ? 'dark' : 'light';
+      this.applyTheme(this.currentTheme);
+    }
+  }
 
   toggleSidebar() {
     this.sidebarToggle.emit();
   }
 
   onThemeChange(event: any) {
-    const theme = event.target.value; // Valor seleccionado (light/dark)
+    const theme = event.target.value;
+    this.applyTheme(theme);
+  }
+
+  private applyTheme(theme: string) {
+    this.currentTheme = theme;
+    if (theme === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
   }
 }
