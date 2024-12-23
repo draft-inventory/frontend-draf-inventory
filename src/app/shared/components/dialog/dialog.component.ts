c5487337e37d61ea105f71c75ed2fca9f0ad3f60
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-12-23 13:34:58 -0500

    feat: set up form component in dialog component

--- src/app/shared/components/dialog/dialog.component.ts
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -1,19 +1,24 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Inject, Input, Output} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {FormComponent} from '../form/form.component';
 
 @Component({
   selector: 'app-dialog',
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatDialogModule, MatButtonModule, FormComponent],
   templateUrl: './dialog.component.html',
   standalone: true,
   styleUrls: ['./dialog.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DialogComponent{
-    constructor(@Inject(MAT_DIALOG_DATA) public data:{
-      title: string;
-      content: string;
-    }) {
-    }
+export class DialogComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { title: string; fields: any[] },
+    private dialogRef: MatDialogRef<DialogComponent>
+  ) {}
+
+  onFormSubmit(formData: any) {
+    console.log('Form Data:', formData);
+    this.dialogRef.close(formData);
+  }
 }
